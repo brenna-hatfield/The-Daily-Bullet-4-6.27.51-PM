2d161ddab8d8bed1bc63c7a965fa387c41e013e3
Brenna Cardona <cis290@PC22766.local>
2017-12-07 19:07:34 -0600

    BH

---
+++ The Daily Bullet 4/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  The Daily Bullet 4
+//
+//  Created by cis290 on 12/7/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sideMenus()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sideMenus() {
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            
+        }
+        
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
